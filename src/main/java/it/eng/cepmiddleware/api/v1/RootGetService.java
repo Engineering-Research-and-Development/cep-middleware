ed45c32e9370170fed71d02bcbd116adc6a1bcf2
Drobniac <drobniac@gmail.com>
2018-05-17 16:49:57 +0200

    Add RootGitService

---
+++ src/main/java/it/eng/cepmiddleware/api/v1/RootGetService.java
@@ -0,0 +1,24 @@
+package it.eng.cepmiddleware.api.v1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import it.eng.cepmiddleware.Service;
+
+public class RootGetService implements Service {
+
+	ResponseEntity<RootGetResponseBody> responseEntity;
+	
+	RootGetService() {
+		this.responseEntity = new ResponseEntity<RootGetResponseBody>(
+			new RootGetResponseBody(),
+			HttpStatus.ACCEPTED
+		);
+	}
+	
+	@Override
+	public  ResponseEntity<?> execute() {
+		return responseEntity;
+	}
+
+}
