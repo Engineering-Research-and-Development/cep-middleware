61cc2a26f215d7df7c538a2e8afe74bfb00b2821
Drobniac <drobniac@gmail.com>
2018-05-17 18:07:29 +0200

    Add PerseoFERule

---
+++ src/main/java/it/eng/cepmiddleware/PerseoFERule.java
@@ -0,0 +1,35 @@
+package it.eng.cepmiddleware;
+
+public class PerseoFERule {
+
+	private String name;
+	private String text;
+	private Object action;
+	
+	public PerseoFERule(CEPRule rule) {
+		this.name = rule.getId();
+		this.text = rule.getStatement();
+		this.action = rule.getAction();
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Object getAction() {
+		return action;
+	}
+
+	public void setAction(Object action) {
+		this.action = action;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
