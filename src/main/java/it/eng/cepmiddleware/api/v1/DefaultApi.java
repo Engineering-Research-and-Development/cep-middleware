4fe03282dbeeed7a65849ce774306c268ce806aa
Drobniac <drobniac@gmail.com>
2018-05-21 11:48:56 +0200

    Clean up DefaultApi and its controller

--- src/main/java/it/eng/cepmiddleware/api/v1/DefaultApi.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/DefaultApi.java
@@ -7,29 +7,17 @@ package it.eng.cepmiddleware.api.v1;
 
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-05-15T12:18:46.813Z")
 
 @Api(value = "default", description = "the default API")
 public interface DefaultApi {
 
-    @ApiOperation(value = "Provides possible operations within the API", nickname = "rootGet", notes = "", tags={ "base", })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "Successful operation") })
-    @RequestMapping(value = "/",
-        method = RequestMethod.GET)
+    @ApiOperation(value = "Provides possible operations within the API")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successful operation")
+    })
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     ResponseEntity<?> rootGet();
 
 }
--- src/main/java/it/eng/cepmiddleware/api/v1/DefaultApiController.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/DefaultApiController.java
@@ -1,15 +1,24 @@
 package it.eng.cepmiddleware.api.v1;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-05-15T12:18:46.813Z")
+import it.eng.cepmiddleware.Service;
 
 @Controller
 public class DefaultApiController implements DefaultApi {
+	
+	private Service service;
+	
+	DefaultApiController() {
+		this(new RootGetService());
+	}
+	
+	DefaultApiController(Service service) {
+		this.service = service;
+	}
 
-    public ResponseEntity<RootGetResponseBody> rootGet() {
-        return new ResponseEntity<RootGetResponseBody>(new RootGetResponseBody(), HttpStatus.ACCEPTED);
+    public ResponseEntity<?> rootGet() {
+        return service.execute();
     }
 
 }
