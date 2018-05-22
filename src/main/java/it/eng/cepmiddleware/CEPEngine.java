1593adcd4604da18f467c2c763d9a27166925770
Drobniac <drobniac@gmail.com>
2018-05-22 12:11:28 +0200

    Update CEPEngine with accept method

--- src/main/java/it/eng/cepmiddleware/CEPEngine.java
+++ src/main/java/it/eng/cepmiddleware/CEPEngine.java
@@ -7,5 +7,6 @@ public interface CEPEngine {
 	public ResponseEntity<?> createRule(CEPRule rule);
 	public ResponseEntity<?> getRule(String ruleId);
 	public ResponseEntity<?> getRules();
+	public void accept(CEPEngineVisitor visitor);
 	
 }
