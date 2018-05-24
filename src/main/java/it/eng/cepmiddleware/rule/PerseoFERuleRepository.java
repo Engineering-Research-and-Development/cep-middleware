f3011a61839ff661988d654278100adb0a5d3c77
Drobniac <drobniac@gmail.com>
2018-05-24 18:03:39 +0200

    Define PerseoFERuleRepository

---
+++ src/main/java/it/eng/cepmiddleware/rule/PerseoFERuleRepository.java
@@ -0,0 +1,5 @@
+package it.eng.cepmiddleware.rule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerseoFERuleRepository extends JpaRepository<PerseoFERule, Long> {}
