e19d994c7bf37d1c34dd15288bb2572b3ef9ea50
Drobniac <drobniac@gmail.com>
2018-05-24 12:09:51 +0200

    Define RuleRepository

---
+++ src/main/java/it/eng/cepmiddleware/rule/RuleRepository.java
@@ -0,0 +1,6 @@
+package it.eng.cepmiddleware.rule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RuleRepository extends JpaRepository<Rule, Long> {}
