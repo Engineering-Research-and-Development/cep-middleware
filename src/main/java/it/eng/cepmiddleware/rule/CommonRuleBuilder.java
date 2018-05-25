3fbe090a3cab515f4a7b5eeb480afe13d47690b3
Drobniac <drobniac@gmail.com>
2018-05-25 18:11:41 +0200

    Create CommonRuleBuilder stub

---
+++ src/main/java/it/eng/cepmiddleware/rule/CommonRuleBuilder.java
@@ -0,0 +1,12 @@
+package it.eng.cepmiddleware.rule;
+
+import java.util.Map;
+
+public class CommonRuleBuilder implements RuleBuilder {
+
+	@Override
+	public Rule build(Map<String, Object> map) throws Exception {
+		return null;
+	}
+
+}
