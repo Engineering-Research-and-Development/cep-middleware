416791dd23cdc68bd4a4d43a850a1599deb0acda
Drobniac <drobniac@gmail.com>
2018-05-25 10:44:13 +0200

    Update RuleRepository

--- src/main/java/it/eng/cepmiddleware/rule/RuleRepository.java
+++ src/main/java/it/eng/cepmiddleware/rule/RuleRepository.java
@@ -1,6 +1,12 @@
 package it.eng.cepmiddleware.rule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RuleRepository extends JpaRepository<Rule, Long> {}
+public interface RuleRepository extends JpaRepository<Rule, Long> {
+	
+	@Query(value = "select r from Rule r where r.ruleId = ?1")
+	Rule getRuleById(String ruleId);
+	
+}
