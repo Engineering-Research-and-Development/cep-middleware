1894ce5285e14aced224958d4a29e6636a66d251
Drobniac <drobniac@gmail.com>
2018-05-25 18:09:32 +0200

    Refactor GetRuleService

--- src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/GetRuleService.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/GetRuleService.java
@@ -1,21 +1,17 @@
 package it.eng.cepmiddleware.api.v1.engine.rules;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import it.eng.cepmiddleware.Service;
-import it.eng.cepmiddleware.engine.CEPEngineFactory;
 import it.eng.cepmiddleware.rule.Rule;
-import it.eng.cepmiddleware.rule.RuleRepository;
+import it.eng.cepmiddleware.rule.RuleCRUDService;
 
 @org.springframework.stereotype.Service
 public class GetRuleService implements Service {
 	
-	@Autowired RuleRepository repository;
-	@Autowired CEPEngineFactory engineFactory;
+	@Autowired RuleCRUDService crudService;
 	ResponseEntity<String> paramError = new ResponseEntity<String>(
 		"Correct parameters not provided",
 		HttpStatus.BAD_REQUEST
@@ -32,10 +28,9 @@ public class GetRuleService implements Service {
 	}
 
 	private ResponseEntity<?> getRuleService(String engineId, String ruleId) {
-		Optional<Rule> maybeRule = Optional.ofNullable(repository.getRuleById(ruleId));
-		return maybeRule.<ResponseEntity<?>>map( (rule) -> {
-			return new ResponseEntity<Rule>(rule, HttpStatus.OK);
-		}).orElse(new ResponseEntity<String>("Rule doesnt exist", HttpStatus.NOT_FOUND));
+		return crudService.read(ruleId).<ResponseEntity<?>>map(
+			rule -> new ResponseEntity<Rule>(rule, HttpStatus.OK)
+		).orElse(new ResponseEntity<String>("Rule doesn't exist", HttpStatus.NOT_FOUND));
 	}
 
 }
