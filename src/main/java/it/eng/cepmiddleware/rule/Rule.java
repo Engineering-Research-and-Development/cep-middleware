d0b2d272e8e5aed329b2e98cc651d89ea66af011
Drobniac <drobniac@gmail.com>
2018-05-25 18:05:36 +0200

    Remove Id getter and setter

--- src/main/java/it/eng/cepmiddleware/rule/Rule.java
+++ src/main/java/it/eng/cepmiddleware/rule/Rule.java
@@ -24,14 +24,6 @@ public class Rule {
 	private String owner;
 	
 	public Rule() {}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getRuleId() {
 		return ruleId;
