fcd4a56aa69a84c90fdbf28fc2955e3b9c64ee30
Drobniac <drobniac@gmail.com>
2018-05-25 18:07:50 +0200

    Change entity mapping approach to un-optimal inheritance

--- src/main/java/it/eng/cepmiddleware/rule/PerseoFERule.java
+++ src/main/java/it/eng/cepmiddleware/rule/PerseoFERule.java
@@ -1,34 +1,16 @@
 package it.eng.cepmiddleware.rule;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
-@Entity(name = "PerseoFERule")
-public class PerseoFERule {
-	
-	@Id
-	@GeneratedValue
-	private Long id;
-	
-	@OneToOne(fetch = FetchType.LAZY)
-    @MapsId
-	private Rule rule;
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class PerseoFERule extends Rule {
 	
 	private String action;
 	
 	public PerseoFERule() {}
-
-	public Rule getRule() {
-		return rule;
-	}
-
-	public void setRule(Rule rule) {
-		this.rule = rule;
-	}
 
 	public String getAction() {
 		return action;
