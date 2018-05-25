9c038c7de213c0bdff187492b2d8518a70ba49b6
Drobniac <drobniac@gmail.com>
2018-05-25 11:41:49 +0200

    Add service stubs for EngineRulesController

---
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/CreateRuleService.java
@@ -0,0 +1,15 @@
+package it.eng.cepmiddleware.api.v1.engine.rules;
+
+import org.springframework.http.ResponseEntity;
+
+import it.eng.cepmiddleware.Service;
+
+public class CreateRuleService implements Service {
+
+	@Override
+	public ResponseEntity<?> execute(Object... parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/RuleIsEnabledService.java
@@ -0,0 +1,16 @@
+package it.eng.cepmiddleware.api.v1.engine.rules;
+
+import org.springframework.http.ResponseEntity;
+
+import it.eng.cepmiddleware.Service;
+
+@org.springframework.stereotype.Service
+public class RuleIsEnabledService implements Service {
+
+	@Override
+	public ResponseEntity<?> execute(Object... parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
