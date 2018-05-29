3fd8074c2613e655aa8ea8784cf896465c62425c
Drobniac <drobniac@gmail.com>
2018-05-29 11:45:18 +0200

    Update PerseoCore

--- src/main/java/it/eng/cepmiddleware/engine/PerseoCore.java
+++ src/main/java/it/eng/cepmiddleware/engine/PerseoCore.java
@@ -1,5 +1,7 @@
 package it.eng.cepmiddleware.engine;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -7,14 +9,22 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import it.eng.cepmiddleware.Converter;
 import it.eng.cepmiddleware.rule.Rule;
 
 public class PerseoCore implements CEPEngine {
 	
 	private String hostUrl;
+	private String name;
 
-	public PerseoCore(String hostUrl) {
+	public PerseoCore(String name, String hostUrl) {
 		this.hostUrl = hostUrl;
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
 	}
 
 	public String getHostUrl() {
@@ -26,10 +36,11 @@ public class PerseoCore implements CEPEngine {
 	}
 	
 	public ResponseEntity<?> createRule(Rule rule) {
+		PerseoCoreRuleAdapter adaptedRule = new PerseoCoreRuleAdapter(rule);
 		try {
 			HttpResponse<Object> response = Unirest.post(hostUrl + "/perseo-core/rules")
 			  .header("accept", "application/json")
-			  .body(rule)
+			  .body(adaptedRule)
 			  .asObject(Object.class);
 			return new ResponseEntity<Object>(
 				response.getBody(),
@@ -67,9 +78,26 @@ public class PerseoCore implements CEPEngine {
 			return new ResponseEntity<Void>(HttpStatus.BAD_GATEWAY);
 		}
 	}
+
 	@Override
-	public void accept(CEPEngineVisitor visitor) {
-		visitor.visit(this);
+	public ResponseEntity<?> deleteRule(String ruleId) {
+		try {
+			System.out.println(hostUrl + "/perseo-core/rules" + ruleId);
+			HttpResponse<Object> response = Unirest.delete(hostUrl + "/perseo-core/rules/" + ruleId)
+			  .header("accept", "application/json")
+			  .asObject(Object.class);
+			return new ResponseEntity<Object>(
+				response.getBody(),
+				HttpStatus.resolve(response.getStatus())
+			);
+		} catch (UnirestException e) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_GATEWAY);
+		}
+	}
+
+	@Override
+	public Converter<? extends Rule, Map<String, Object>> getConverter() {
+		return new PerseoCoreConverter(this.getName());
 	}
 	
 }
