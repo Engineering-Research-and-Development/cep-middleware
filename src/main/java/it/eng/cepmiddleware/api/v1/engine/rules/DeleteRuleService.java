a5c0df0ff849f09eac457cd46fde6e5b2f1660d4
Drobniac <drobniac@gmail.com>
2018-05-29 18:00:27 +0200

    Implement DeleteRuleService

--- src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/DeleteRuleService.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/DeleteRuleService.java
@@ -1,16 +1,39 @@
 package it.eng.cepmiddleware.api.v1.engine.rules;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import it.eng.cepmiddleware.Service;
+import it.eng.cepmiddleware.rule.RuleCRUDService;
 
 @org.springframework.stereotype.Service
 public class DeleteRuleService implements Service {
 
+	@Autowired RuleCRUDService crudService;
+
+	ResponseEntity<String> paramError = new ResponseEntity<String>(
+		"Correct parameters not provided",
+		HttpStatus.BAD_REQUEST
+	);
+
 	@Override
 	public ResponseEntity<?> execute(Object... parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		if (parameters[0] instanceof String && parameters[1] instanceof String) {
+			String engineId = (String) parameters[0];
+			String ruleId = (String) parameters[1];
+			return deleteRuleService(engineId, ruleId);
+		}
+		return paramError;
+	}
+
+	private ResponseEntity<?> deleteRuleService(String engineId, String ruleId) {
+		try {
+			crudService.delete(ruleId);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok().build();
 	}
 
 }
