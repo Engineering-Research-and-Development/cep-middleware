c3a7ae867e411141f375d5a8aa9dc9c8c811980a
Drobniac <drobniac@gmail.com>
2018-05-29 11:36:00 +0200

    Update RuleCRUDService create implementation

--- src/main/java/it/eng/cepmiddleware/rule/RuleCRUDService.java
+++ src/main/java/it/eng/cepmiddleware/rule/RuleCRUDService.java
@@ -18,11 +18,17 @@ public class RuleCRUDService implements CRUDService<Rule, String> {
 
 	@Override
 	public void create(Rule rule) throws Exception {
-		CEPEngine engine = engineFactory.getCEPEngine(rule.getOwner());
-		if (/*engine.createRule(rule).getStatusCode().is2xxSuccessful()*/ true) {
-			repository.save(rule);
+		if (rule.getRuleId().equals(Rule.defaultRuleId)) {
+			CEPEngine engine = engineFactory.getCEPEngine(rule.getOwner());
+			if (engine.createRule(rule).getStatusCode().is2xxSuccessful()) {
+				repository.save(rule);
+				engine.deleteRule(Rule.defaultRuleId);
+				engine.createRule(rule);
+			} else {
+				throw new Exception(String.format("The %s engine couldn't save your rule", rule.getOwner()));
+			}
 		} else {
-			throw new Exception(String.format("The %s engine couldn't save your rule", rule.getOwner()));
+			throw new Exception("Didn't expect non-default ruleId");
 		}
 	}
 
