7363a19a8f88edbd939b7fa1b8aeae1e8942fbf1
Drobniac <drobniac@gmail.com>
2018-05-30 11:55:47 +0200

    Update CRUDService

--- src/main/java/it/eng/cepmiddleware/CRUDService.java
+++ src/main/java/it/eng/cepmiddleware/CRUDService.java
@@ -8,7 +8,7 @@ public interface CRUDService<T, ID> {
 	public void create(T entity) throws Exception;
 	public Optional<T> read(ID entityId);
 	public Collection<T> read();
-	public T update(T entity);
+	public void update(T entity) throws Exception;
 	public void delete(ID entityId) throws Exception;
 
 }
