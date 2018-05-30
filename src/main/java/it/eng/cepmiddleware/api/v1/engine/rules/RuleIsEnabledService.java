b513eb3fe00f66368100beab21ef0047e2ccaa03
Drobniac <drobniac@gmail.com>
2018-05-30 15:21:48 +0200

    Implemented RuleIsEnabledService

--- src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/RuleIsEnabledService.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/RuleIsEnabledService.java
@@ -1,16 +1,45 @@
 package it.eng.cepmiddleware.api.v1.engine.rules;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import it.eng.cepmiddleware.Service;
+import it.eng.cepmiddleware.engine.CEPEngineFactory;
+import it.eng.cepmiddleware.rule.RuleCRUDService;
 
 @org.springframework.stereotype.Service
 public class RuleIsEnabledService implements Service {
 
+	@Autowired RuleCRUDService crudService;
+	@Autowired GetRuleService getRuleService;
+	@Autowired CEPEngineFactory engineFactory;
+
+	ResponseEntity<String> paramError = new ResponseEntity<String>(
+		"Correct parameters not provided",
+		HttpStatus.BAD_REQUEST
+	);
+
 	@Override
 	public ResponseEntity<?> execute(Object... parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		if (parameters[0] instanceof String && parameters[1] instanceof String) {
+			String engineId = (String) parameters[0];
+			String ruleId = (String) parameters[1];
+			return ruleIsEnabled(engineId, ruleId);
+		}
+		return paramError;
+	}
+
+	private ResponseEntity<?> ruleIsEnabled(String engineId, String ruleId) {
+		ResponseEntity<?> getResponse =  getRuleService.execute(engineId, ruleId);
+		ResponseEntity<?> ruleResponse = engineFactory.getCEPEngine(engineId).getRule(ruleId);
+		if (getResponse.getStatusCode().is2xxSuccessful()) {
+			if (ruleResponse.getStatusCode().is2xxSuccessful()) {
+				return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+			}
+			return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+		}
+		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 	}
 
 }
