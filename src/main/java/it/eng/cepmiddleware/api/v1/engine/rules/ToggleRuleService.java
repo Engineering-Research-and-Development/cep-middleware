5d72234f4993134fe28b48f9f0dd349b055c92f3
Drobniac <drobniac@gmail.com>
2018-05-30 16:25:19 +0200

    Implement ToggleRuleService

--- src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/ToggleRuleService.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/rules/ToggleRuleService.java
@@ -1,16 +1,54 @@
 package it.eng.cepmiddleware.api.v1.engine.rules;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import it.eng.cepmiddleware.Service;
+import it.eng.cepmiddleware.engine.CEPEngineFactory;
+import it.eng.cepmiddleware.rule.RuleCRUDService;
 
 @org.springframework.stereotype.Service
 public class ToggleRuleService implements Service {
+	
+	@Autowired RuleIsEnabledService ruleIsEnabledService;
+	@Autowired RuleCRUDService ruleCRUDService;
+	@Autowired CEPEngineFactory engineFactory;
+	
+	ResponseEntity<String> paramError = new ResponseEntity<String>(
+		"Correct parameters not provided",
+		HttpStatus.BAD_REQUEST
+	);
 
 	@Override
 	public ResponseEntity<?> execute(Object... parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		if (
+			parameters[0] instanceof String &&
+			parameters[1] instanceof String &&
+			parameters[2] instanceof Boolean
+		) {
+			String engineId = (String) parameters[0];
+			String ruleId = (String) parameters[1];
+			Boolean enableSwitch = (Boolean) parameters[2];
+			return toggleRule(engineId, ruleId, enableSwitch);
+		}
+		return paramError;
+	}
+
+	private ResponseEntity<?> toggleRule(String engineId, String ruleId, Boolean enableSwitch) {
+		ResponseEntity<?> ruleEnabledResponse = ruleIsEnabledService.execute(engineId, ruleId);
+		if (ruleEnabledResponse.getStatusCode().is2xxSuccessful()) {
+			if (enableSwitch) {
+				ruleCRUDService.read(ruleId).<ResponseEntity<?>>map(rule -> {
+					return engineFactory.getCEPEngine(engineId).createRule(rule);
+				}).orElse(new ResponseEntity<>("Couldn't switch the rule on :'(", HttpStatus.BAD_REQUEST));
+			} else {
+				return engineFactory.getCEPEngine(engineId).deleteRule(ruleId).getStatusCode().is2xxSuccessful()?
+						new ResponseEntity<>("Rule disabled", HttpStatus.OK):
+						new ResponseEntity<>("Disable failed", HttpStatus.BAD_REQUEST);
+			}
+		}
+		return ruleEnabledResponse;
 	}
 
 }
