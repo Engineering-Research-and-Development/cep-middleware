4391dc38155ef7be3feb0dba20fbf420cbaab491
Drobniac <drobniac@gmail.com>
2018-06-01 14:06:51 +0200

    Fix configuration issues

--- src/main/java/it/eng/cepmiddleware/config/CEPMiddlewareConfiguration.java
+++ src/main/java/it/eng/cepmiddleware/config/CEPMiddlewareConfiguration.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,7 +25,8 @@ public class CEPMiddlewareConfiguration {
 	@Value("${path.perseo-fe}")
 	String perseoFEPath;
 	
-	public CEPMiddlewareConfiguration() {
+	@PostConstruct
+	private void init() {
 		this.cepEngines = new HashMap<String, CEPEngine>();
 		this.cepEngines.put("perseo-core", new PerseoCore("perseo-core", perseoCorePath));
 		this.cepEngines.put("perseo-fe", new PerseoFrontEnd("perseo-fe", perseoFEPath));
--- src/main/java/it/eng/cepmiddleware/engine/CachedEngineFactory.java
+++ src/main/java/it/eng/cepmiddleware/engine/CachedEngineFactory.java
@@ -1,5 +1,6 @@
 package it.eng.cepmiddleware.engine;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import it.eng.cepmiddleware.config.CEPMiddlewareConfiguration;
@@ -7,11 +8,7 @@ import it.eng.cepmiddleware.config.CEPMiddlewareConfiguration;
 @Component
 public class CachedEngineFactory implements CEPEngineFactory {
 
-	private CEPMiddlewareConfiguration config;
-	
-	CachedEngineFactory() {
-		config = new CEPMiddlewareConfiguration();
-	}
+	@Autowired private CEPMiddlewareConfiguration config;
 	
 	@Override
 	public CEPEngine getCEPEngine(String engineId) {
