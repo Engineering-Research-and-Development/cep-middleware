44630433ca66bcd9a8401c21d960d54bc0a06197
Drobniac <drobniac@gmail.com>
2018-06-22 16:08:03 +0200

    Add mocks for engine CRUD operations

--- src/main/java/it/eng/cepmiddleware/api/v1/engine/EngineApiController.java
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/EngineApiController.java
@@ -1,12 +1,14 @@
 package it.eng.cepmiddleware.api.v1.engine;
 
-import io.swagger.annotations.*;
 import it.eng.cepmiddleware.Service;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class EngineApiController implements EngineApi {
@@ -14,12 +16,31 @@ public class EngineApiController implements EngineApi {
 	@Autowired private Service getEnginesService;
 	@Autowired private Service getEngineService;
 
-    public ResponseEntity<?> getEngine(@ApiParam(value = "",required=true) @PathVariable("engineId") String engineId) {
+	public ResponseEntity<?> getEngine(@PathVariable("engineId") String engineId) {
         return getEngineService.execute(engineId);
     }
 
     public ResponseEntity<?> getEngines() {
         return getEnginesService.execute();
     }
+
+	public ResponseEntity<?> deleteEngine(@PathVariable("engineId") String engineId) {
+		return ResponseEntity.ok().build();
+	}
+
+	@Override
+	public ResponseEntity<?> addEngine(
+		@Valid @RequestBody Object engineInfo
+	) {
+		return ResponseEntity.ok().build();
+	}
+
+	@Override
+	public ResponseEntity<?> updateEngine(
+		@PathVariable("engineId") String engineId,
+		@Valid @RequestBody Object engineInfo
+	) {
+		return ResponseEntity.ok().build();
+	}
 
 }
