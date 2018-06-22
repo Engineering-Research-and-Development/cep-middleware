a9a119088ca7daf59847b24814ce06934edb8379
Drobniac <drobniac@gmail.com>
2018-06-22 13:21:05 +0200

    Create EngineBuildToken

---
+++ src/main/java/it/eng/cepmiddleware/config/EngineBuildToken.java
@@ -0,0 +1,33 @@
+package it.eng.cepmiddleware.config;
+
+class EngineBuildToken {
+
+	private String engineId;
+	private String engineType;
+	private String hostUrl;
+
+	public String getEngineId() {
+		return engineId;
+	}
+
+	public void setEngineId(String engineId) {
+		this.engineId = engineId;
+	}
+
+	public String getEngineType() {
+		return engineType;
+	}
+
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+
+	public String getHostUrl() {
+		return hostUrl;
+	}
+
+	public void setHostUrl(String hostUrl) {
+		this.hostUrl = hostUrl;
+	}
+
+}
