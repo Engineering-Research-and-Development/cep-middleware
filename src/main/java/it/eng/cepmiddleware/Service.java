e82f68c0ef1ec23afd2a68c1cca61e42192d968f
Drobniac <drobniac@gmail.com>
2018-07-04 17:23:02 +0200

    Update it.eng.cepmiddleware.Service

--- src/main/java/it/eng/cepmiddleware/Service.java
+++ src/main/java/it/eng/cepmiddleware/Service.java
@@ -1,8 +1,16 @@
 package it.eng.cepmiddleware;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @FunctionalInterface
 public interface Service {
+
+	ResponseEntity<String> paramError = new ResponseEntity<String>(
+		"Correct parameters not provided",
+		HttpStatus.BAD_REQUEST
+	);
+
 	public ResponseEntity<?> execute(Object ...parameters);
+
 }
