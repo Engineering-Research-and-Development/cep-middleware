cfc50a6eece332988bc06680166e54f818890317
Drobniac <drobniac@gmail.com>
2018-07-04 17:32:10 +0200

    Implement UpdateEngineService

---
+++ src/main/java/it/eng/cepmiddleware/api/v1/engine/UpdateEngineService.java
@@ -0,0 +1,37 @@
+package it.eng.cepmiddleware.api.v1.engine;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import it.eng.cepmiddleware.Service;
+import it.eng.cepmiddleware.config.CEPEngineConfiguration;
+import it.eng.cepmiddleware.engine.CEPEngine;
+
+@org.springframework.stereotype.Service
+public class UpdateEngineService implements Service {
+	
+	@Autowired MapToEngineConverter converter;
+	@Autowired CEPEngineConfiguration engineConfig;
+
+	@Override
+	public ResponseEntity<?> execute(Object... parameters) {
+		if (parameters[0] instanceof Map) {
+			Map<String, String> engineInfo = (Map<String, String>)parameters[0];
+			return updateEngine(engineInfo);
+		}
+		return Service.paramError;
+	}
+
+	private ResponseEntity<?> updateEngine(Map<String, String> engineInfo) {
+		CEPEngine engine = converter.convert(engineInfo);
+		if (engineConfig.getCepEngines().get(engine.getName()) == null) {
+			return ResponseEntity.notFound().build();
+		}
+		engineConfig.getCepEngines().put(engine.getName(), engine);
+		return ResponseEntity.ok().build();
+	}
+
+}
