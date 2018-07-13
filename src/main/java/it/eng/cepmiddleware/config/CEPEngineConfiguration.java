8a7b304a8a188ae284b9792399c1b4ee84d67f09
Drobniac <drobniac@gmail.com>
2018-07-13 17:05:00 +0200

    Update CEPEngineConfiguration

--- src/main/java/it/eng/cepmiddleware/config/CEPEngineConfiguration.java
+++ src/main/java/it/eng/cepmiddleware/config/CEPEngineConfiguration.java
@@ -12,31 +12,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import it.eng.cepmiddleware.engine.CEPEngine;
-import it.eng.cepmiddleware.engine.ErrorCEPEngine;
 import it.eng.cepmiddleware.engine.perseo.Perseo;
 
 @Configuration
 @ConfigurationProperties()
 public class CEPEngineConfiguration {
 
-	private Map<EngineInfoToken, CEPEngine> cepEngines = new HashMap<>();
+	private Map<String, CEPEngine> cepEngines = new HashMap<>();
+	private Map<String, ImmutableEngineInfoToken> cepEnginesInfo = new HashMap<>();
 	private Map<String, Class> supportedEngines = new HashMap<>();
 
 	CEPEngineConfiguration() {
 		supportedEngines.put("Perseo", Perseo.class);
 	}
-	
+
 	@Bean()
 	@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	CEPEngine engine(EngineInfoToken buildToken) throws Exception {
+		String engineId = buildToken.getEngineId();
+		String hostUrl = buildToken.getHostUrl();
+		if (engineId == null || hostUrl == null) {
+			throw new Exception("engineId or hostUrl were not provided");
+		}
 		return ((CEPEngine)supportedEngines.get(
 			buildToken.getEngineType()
 		).getConstructor(
 			String.class,
 			String.class
 		).newInstance(
-			buildToken.getEngineId(),
-			buildToken.getHostUrl()
+			engineId,
+			hostUrl
 		));
 	}
 
@@ -44,38 +49,42 @@ public class CEPEngineConfiguration {
 		for (int i = 0 ; i < tokens.length ; i++) {
 			try {
 				CEPEngine engine = engine(tokens[i]);
-				cepEngines.put(
-					tokens[i],
-					engine
-				);
+				String engineId = tokens[i].getEngineId();
+				cepEngines.put(engineId, engine);
+				cepEnginesInfo.put(engineId, new ImmutableEngineInfoToken(tokens[i]));
 			} catch (Exception e) {
 				System.out.println("Couldn't create an engine using engines[" + i + "]");
 			}
 		}
 	}
 
-	public Collection<EngineInfoToken> getEnginesInfo() {
-		return cepEngines.keySet();
+	public Collection<ImmutableEngineInfoToken> getEnginesInfo() {
+		return cepEnginesInfo.values();
 	}
 
-	public Optional<EngineInfoToken> getEngineInfo(String engineId) {
-		return cepEngines.keySet().stream().filter((elem) -> {
-			return elem.getEngineId().equals(engineId);
-		}).findFirst();
+	public Optional<ImmutableEngineInfoToken> getEngineInfo(String engineId) {
+		return Optional.ofNullable(cepEnginesInfo.get(engineId));
 	}
 
-	public Map<EngineInfoToken, CEPEngine> getCepEngines() {
-		return this.cepEngines;
+	public Collection<CEPEngine> getCepEngines() {
+		return this.cepEngines.values();
 	}
-	
+
 	public Optional<CEPEngine> getCepEngine(String engineId) {
-		return Optional.ofNullable(this.cepEngines.get(
-			getEngineInfo(engineId).orElse(null)
-		));
-	}
-	
-	public CEPEngine putCepEngine(EngineInfoToken token) throws Exception {
-		return cepEngines.put(token, engine(token));
+		return Optional.ofNullable(this.cepEngines.get(engineId));
 	}
 
+	public CEPEngine putCepEngine(ImmutableEngineInfoToken token) throws Exception {
+		CEPEngine engine = this.engine(new EngineInfoToken(token));
+		String engineId = token.getEngineId();
+		this.cepEngines.put(engineId, engine);
+		this.cepEnginesInfo.put(engineId, token);
+		return engine;
+	}
+
+	public CEPEngine remove(String engineId) {
+		CEPEngine deletedEngine = this.cepEngines.remove(engineId);
+		this.cepEnginesInfo.remove(engineId);
+		return deletedEngine;
+	}
 }
