6ff0307d21676685d3611cd30f794757338a14e9
Drobniac <drobniac@gmail.com>
2018-08-08 11:26:10 +0200

    Disable Spring Security auto-configuration

--- src/main/java/it/eng/cepmiddleware/CEPMiddlewareApplication.java
+++ src/main/java/it/eng/cepmiddleware/CEPMiddlewareApplication.java
@@ -3,10 +3,11 @@ package it.eng.cepmiddleware;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import com.mashape.unirest.http.Unirest;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CEPMiddlewareApplication {
 
 	public static void main(String[] args) {
